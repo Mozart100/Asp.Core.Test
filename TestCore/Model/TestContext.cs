3326125725383fa3fa2bd37a359f38a604d546b5
Anatoliy Ark <anatoliy.arkove@gmail.com>
2018-01-19 13:11:58 +0200

    Skeleton

---
+++ TestCore/Model/TestContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestCore.Model
+{
+    public class TestContext : DbContext
+    {
+        public TestContext(DbContextOptions<TestContext> options) : base(options)
+        {
+        }
+
+        //public DbSet<Course> Courses { get; set; }
+        //public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Student> Students { get; set; }
+    }
+}
